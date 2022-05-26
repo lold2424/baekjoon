1ee7d0b6d60d90a109abd5fca2ac72b5a35a144b
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-05-26 10:20:31 +0900

    [Bronze II] Title: 분해합, Time: 144 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,44 +1,33 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.io.IOException;
+ 
 public class Main {
-	
-	public static int arr[];
-	
 	public static void main(String[] args) throws IOException {
-		
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+        
 		int N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		int i;
-		int cnt = 0;
+        
+		int result = 0;
+ 
 		
-		for(i = 1; i <= N ; i++) {
+		for(int i = 0; i < N; i++) {
+			int number = i;
 			int sum = 0;
-			int count = i;
 			
-			while(count != 0) {
-				sum += count % 10;
-				count /= 10;
+			while(number != 0) {
+				sum += number % 10;
+				number /= 10;
 			}
-			if(i + sum == N) {
-				arr[cnt] = i;
-				cnt++;
+			
+			if(sum + i == N) {
+				result = i;
+				break;
 			}
+			
 		}
-		int solution = N;
-		
-		for(i = 0; i < N; i ++) {
-			if(solution > arr[i]) {
-				if(arr[i] == 0) {
-					break;
-				}
-				solution = arr[i];
-			}
-		}
-		if(solution == N)	solution = 0;
-		System.out.println(solution);
+ 
+		System.out.println(result);
 	}
 }
