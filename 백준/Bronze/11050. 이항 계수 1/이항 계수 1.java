8702f45fb3481cd810f3e41c3692a39485ffc63e
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-08-20 22:47:46 +0900

    [Bronze I] Title: 이항 계수 1, Time: 204 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int bunja = 1;
+        int factorial = 1;
+     
+        for(int i=n; i>n-k; i--) {
+            bunja *= i;
+        }
+
+        for(int j=1; j<=k; j++) {
+            factorial *= j;
+        }
+        System.out.println(bunja/factorial);
+    }     
+}
