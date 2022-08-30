b9de8818699dd295bd1cefbe3ba445f32c161146
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-08-30 16:53:59 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,50 +1,33 @@
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
+
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	
-	static int[][] dp = new int[30][30];
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st;
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < T; i++) {
-			
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			// M개중 N개를 뽑는 경우이므로 nCr 에서 n = M, r = N이다.
-			int N = Integer.parseInt(st.nextToken());	// N = r
-			int M = Integer.parseInt(st.nextToken());	// M = n
-					
-			
-			sb.append(combi(M, N)).append('\n');
-		}
-		
-		System.out.println(sb);
- 
-	}
-	
-	static int combi(int n, int r) {
-		
-		// 이미 풀린 경우 바로 반환
-		if(dp[n][r] > 0) {
-			return dp[n][r]; 
-		}
-		
-		// 2번 성질
-		if(n == r || r == 0) {
-			return dp[n][r] = 1;
-		}
-		
-		// 1번 성질
-		return dp[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < testCase; i++){
+            String [] riverSite = br.readLine().split(" ");
+            long westSite = Long.parseLong(riverSite[0]);
+            long eastSite = Long.parseLong(riverSite[1]);
+            long result = 1;
+
+            for(int j = 0; j < westSite; j++){
+                result *= (eastSite - j);
+                result /= (j + 1);
+            }
+
+            bw.write(result + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
