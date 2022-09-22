e7ad01f6d21d0d52bb51e0e53c951159cccd1d48
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-09-22 20:12:30 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0)  sum += i;
+        }
+        int answer = sum;
+        return answer;
+    }
+}
