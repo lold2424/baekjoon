825134481eb54c0ec7cbe9fe96818fa2286f3858
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-11-17 13:25:38 +0900

    [Gold V] Title: 평범한 배낭, Time: 208 ms, Memory: 53780 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -11,10 +11,10 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		int kg = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
-		int dp[][] = new int[N+1][kg+1];
-		int product[][] = new int[N+1][2];
+		int dp[][] = new int[N + 1][K + 1];
+		int product[][] = new int[N + 1][2];
         
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -23,16 +23,16 @@ public class Main {
 		}
 		
 		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= kg; j++) {
+			for (int j = 1; j <= K; j++) {
 				
 				if (j - product[i][0] >= 0) {
-				  dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-product[i][0]] + product[i][1]);
+				  dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j-product[i][0]] + product[i][1]);
 				} else {
-					dp[i][j] = dp[i-1][j];
+					dp[i][j] = dp[i - 1][j];
 				}
 			}
 		}
 		
-		System.out.println(dp[N][kg]);
+		System.out.println(dp[N][K]);
 	}
 }
