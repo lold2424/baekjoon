0a90806b07e6be2ebca0676f993001499d1b4346
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-12-09 15:05:23 +0900

    [Silver II] Title: 삼각형으로 자르기, Time: 132 ms, Memory: 14888 KB -BaekjoonHub

--- 백준/Silver/1198. 삼각형으로 자르기/삼각형으로 자르기.java
+++ 백준/Silver/1198. 삼각형으로 자르기/삼각형으로 자르기.java
@@ -1,36 +1,48 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int[][] point;
-	public static void main(String[] args) throws IOException {
+	
+	static int[][] arr;
+	static int[] list = new int[3];
+	static int s, N;
+	
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		point = new int[N][2];
-		for(int i=0; i<N; ++i) {
-			st = new StringTokenizer(br.readLine());
-			point[i][0] = Integer.parseInt(st.nextToken());
-			point[i][1] = Integer.parseInt(st.nextToken());
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][2];
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		int max = 0;
-		for(int i=0; i<N-2; ++i) {
-			for(int j=i+1; j<N-1; ++j) {
-				for(int k=j+1; k<N; ++k) {
-					max = Math.max(max, area(i,j,k));
-				}
-			}
-		}
-		System.out.print(sb.append(max/2).append(max%2==0?".0":".5"));
+		s = 0;
+		dfs(0, 0);
+		System.out.println((double) s / 2.0);
 	}
-	static int area(int a, int b, int c) {
-		int n1 = point[a][0]*point[b][1]+point[b][0]*point[c][1]+point[c][0]*point[a][1];
-		int n2 = point[a][1]*point[b][0]+point[b][1]*point[c][0]+point[c][1]*point[a][0];
-		return Math.abs(n1-n2);
+	
+	private static void dfs(int t, int k) {
+		if (k == 3) {
+			int tmp = 0;
+			int a = list[0];
+			int b = list[1];
+			int c = list[2];
+			tmp += arr[a][0] * arr[b][1] + arr[b][0] * arr[c][1] + arr[c][0] * arr[a][1];
+			tmp -= arr[b][0] * arr[a][1] + arr[c][0] * arr[b][1] + arr[a][0] * arr[c][1];
+			tmp = Math.abs(tmp);
+			s = Math.max(s, tmp);
+			return;
+		}
+		
+		for (int i = t; i < N; i++) {
+			list[k] = i;
+			dfs(i + 1, k + 1);
+		}
 	}
 }
