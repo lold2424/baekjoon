9533e307effb94aaf473b900d09e6a732320da30
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-12-16 19:29:05 +0900

    [Bronze I] Title: Meats On The Grill, Time: 120 ms, Memory: 13980 KB -BaekjoonHub

--- 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.java
+++ 백준/Bronze/10219. Meats On The Grill/Meats On The Grill.java
@@ -1,21 +1,26 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-         
-        int n = Integer.parseInt(br.readLine());
-        for(int i = 0; i < n; i++) {
-            int h =  Integer.parseInt(br.readLine().split(" ")[0]);
-            for(int j = 0; j < h; j++) {
-                sb.append(new StringBuilder(br.readLine()).reverse()).append("\n");
-            }
-        }
-         
-        bw.write(sb.toString());
-        bw.close();
-    }
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(reader.readLine());
+		for (int i = 0; i < T; i++) {
+			StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+			int H = Integer.parseInt(tokenizer.nextToken());
+			int W = Integer.parseInt(tokenizer.nextToken());
+
+			StringBuilder builder = new StringBuilder();
+			for (int j = 0; j < H; j++) {
+				builder.append(new StringBuilder(reader.readLine()).reverse());
+				builder.append("\n");
+			}
+			System.out.println(builder.toString());
+		}
+	}
 }
