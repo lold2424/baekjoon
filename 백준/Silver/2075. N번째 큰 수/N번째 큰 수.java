3d419c9a0124145f4e59ac799d6ce8ff44ebc95f
윤혜진 <91832324+lold2424@users.noreply.github.com>
2022-12-17 13:01:01 +0900

    [Silver II] Title: N번째 큰 수, Time: 1560 ms, Memory: 228968 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,25 +1,29 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+import java.util.StringTokenizer;
+import java.util.Arrays;
+ 
 class Main {
-    
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
- 
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n*n];
-        int idx =0;
- 
-        for(int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++) {
-                arr[idx++] = Integer.parseInt(st.nextToken());
-            }
-        }
- 
-        Arrays.sort(arr);
- 
-        System.out.println(arr[n*n - n]);
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n*n];
+		int idx =0;
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				arr[idx ++] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[n * n - n]);
+	}
 }
