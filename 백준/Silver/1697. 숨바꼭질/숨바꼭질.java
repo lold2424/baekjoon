145a04adc1495ddaade8ecdca9d549653e6178e7
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-01-14 21:07:51 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 18072 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -7,55 +7,56 @@ import java.util.Queue;
 import java.util.StringTokenizer;
  
 class Main {
-    static int stoi(String str) {
-        return Integer.parseInt(str);
-    }
- 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        int N = stoi(st.nextToken());
-        int K = stoi(st.nextToken());
- 
-        if(N >= K)
-            System.out.println(N-K);
-        else
-            System.out.println(bfs(N, K));
-    }
- 
-    static int bfs(int n, int k) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        int[] subin = new int[100001];
- 
-        q.offer(n);
-        subin[n] = 1;
- 
-        while(!q.isEmpty()) {
-            int now = q.poll();
- 
-            for(int i=0; i<3; i++) {
-                int next;
- 
-                if(i == 0)
-                    next = now - 1;
-                else if(i == 1)
-                    next = now + 1;
-                else
-                    next = now * 2;
- 
-                if(next == k)
-                    return subin[now];                
- 
-                if(0 <= next && next <= 100000) {
-                    if(subin[next] == 0) {
-                        q.offer(next);
-                        subin[next] = subin[now] + 1;
-                    }
-                }
-            }
-        }
- 
-        return 0;
-    }
+	
+	static Queue<Integer> q = new LinkedList<Integer>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		if(N >= K) {
+			System.out.println(N - K);
+		} else {
+			System.out.println(bfs(N, K));
+		}
+	}
+	
+	static int bfs(int n, int k) {
+		int[] subin = new int[100001];
+		
+		q.offer(n);
+		subin[n] = 1;
+		
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			
+			for(int i = 0; i < 3; i++) {
+				int next;
+				
+				if(i == 0) {
+					next = now - 1;
+				} else if(i == 1) {
+					next = now + 1;
+				} else {
+					next = now * 2;
+				}
+				
+				if(next == k) {
+					return subin[now];
+				}
+				
+				if(0 <= next && next <= 100000) {
+					if(subin[next] == 0) {
+						q.offer(next);
+						subin[next] = subin[now] + 1;
+					}
+				}
+			}
+		}
+		
+		return 0;
+	}
 }
