b4d3e8e850b077c2c47d86f88c3b328928924789
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-01-28 18:11:29 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
 	
 	public static void main(String[] args)throws IOException {
