9ccee9202a08af3e22545237f745483917bb626e
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-01-29 19:18:24 +0900

    [Silver III] Title: 후위 표기식2, Time: 132 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Stack;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Double> operand = new Stack<>();
+        
+        int N = Integer.parseInt(br.readLine());
+        String prefix = br.readLine();
+
+        int[] arr = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        
+        int len = prefix.length();
+        
+        for(int i = 0; i < len; i++) {
+            char ch = prefix.charAt(i);
+            if('A' <= ch && ch <= 'Z') {
+                double d = arr[ch - 'A'];
+                operand.push(d);
+            } else {
+                double d1 = operand.pop();
+                double d2 = operand.pop();
+                double d3 = 0.0;
+                switch(ch) {
+                    case '+' :
+                        d3 = d2 + d1;
+                        break;
+                    case '-' :
+                        d3 = d2 - d1;
+                        break;
+                    case '*' :
+                        d3 = d2 * d1;
+                        break;
+                    case '/' :
+                        d3 = d2 / d1;
+                        break;
+                }
+                operand.push(d3);
+            }
+        }
+        
+        System.out.printf("%.2f", operand.pop());
+    }
+}
