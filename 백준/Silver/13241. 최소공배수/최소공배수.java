bf2653a33ff637241b529a9ea27122feac0570d9
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-02-14 19:45:28 +0900

    [Silver V] Title: 최소공배수, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.java
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -1,24 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        long a = input.nextLong();
-        long b = input.nextLong();
-
-        long gcd = getGCD(a, b);
-        long lcm = a / gcd * b; // formula for LCM
-
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        
+        long gcd = getGCD(A, B);
+        long lcm = A / gcd * B;
+        
         System.out.println(lcm);
     }
 
-    // Euclidean algorithm to find greatest common divisor (GCD)
     private static long getGCD(long a, long b) {
         while (b > 0) {
             long temp = b;
             b = a % b;
             a = temp;
         }
+        
         return a;
     }
 }
