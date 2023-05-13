145fb0503508a7f76e6df03aa358c531047d5573
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-05-13 16:38:58 +0900

    [Silver III] Title: 피보나치 함수, Time: 240 ms, Memory: 18500 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,40 +1,18 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.util.Scanner;
+
 public class Main {
-	
-	static Integer[][] dp = new Integer[41][2];
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		dp[0][0] = 1;
-		dp[0][1] = 0;
-		dp[1][0] = 0;
-		dp[1][1] = 1;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		while (T --> 0){
-			
-			int N = Integer.parseInt(br.readLine());
-			fibonacci(N);
-			sb.append(dp[N][0] + " " + dp[N][1]).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
-	
-	static Integer[] fibonacci(int N) {
-		
-		if (dp[N][0] == null || dp[N][1] == null) {
-			
-			dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
-			dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
-		}
-		
-		return dp[N];
-	}
+	static int[][] Fibo = new int[41][2];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int Test_Case = sc.nextInt();
+		Fibo[0][0] = 1; Fibo[0][1] = 0;
+		Fibo[1][0] = 0; Fibo[1][1] = 1;
+		for(int i = 2; i < 41; i++) {
+			Fibo[i][0] = Fibo[i-2][0] + Fibo[i-1][0];
+			Fibo[i][1] = Fibo[i-2][1] + Fibo[i-1][1];
+				}
+		for(int i = 0; i < Test_Case; i++) { 
+			int A = sc.nextInt(); System.out.println(Fibo[A][0] + " " + Fibo[A][1]); 
+		} 
+	} 
 }
