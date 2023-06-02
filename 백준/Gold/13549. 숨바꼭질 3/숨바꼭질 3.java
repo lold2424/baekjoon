7e612ecd294776f330e803dc03f069ebf4a270d7
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-06-02 11:01:00 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 272 ms, Memory: 87016 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
