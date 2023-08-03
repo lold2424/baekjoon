db5cd90b9283fc7a81e5db2dd28420b8a8a5e420
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-08-03 19:43:25 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 1748 ms, Memory: 60348 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -3,20 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            String[] words = br.readLine().split(" ");
+		for (int i = 0; i < n; i++) {
+			String[] words = br.readLine().split(" ");
 
-            for (String word : words) {
-                StringBuilder sb = new StringBuilder(word);
-                System.out.print(sb.reverse().toString() + " ");
-            }
-            System.out.println();
-        }
-        br.close();
-    }
+			for (String word : words) {
+				StringBuilder sb = new StringBuilder(word);
+				System.out.print(sb.reverse().toString() + " ");
+			}
+
+			System.out.println();
+		}
+	}
 }
