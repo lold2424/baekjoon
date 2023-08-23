0b87e50c6c1cd349722982068a244115b2619b10
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-08-23 17:01:46 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 604 ms, Memory: 51804 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
@@ -4,60 +4,66 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] arr;
-	static int[] tree;
+	static int[] originalArray;
+	static int[] segmentTree;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		arr = new int[n + 1];
-		tree = new int[n * 4];
+
+		originalArray = new int[n + 1];
 		for (int i = 1; i <= n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			originalArray[i] = Integer.parseInt(st.nextToken());
 		}
-		init(1, n, 1);
+
+		segmentTree = new int[n * 4];
+		initSegmentTree(1, n, 1);
+
 		int m = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			if (a == 1) {
-				int b = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
-				arr[b] = c;
-				update(1, n, 1, b);
+			int queryType = Integer.parseInt(st.nextToken());
+
+			if (queryType == 1) {
+				int index = Integer.parseInt(st.nextToken());
+				int value = Integer.parseInt(st.nextToken());
+				originalArray[index] = value;
+				updateSegmentTree(1, n, 1, index);
 			} else {
-				sb.append(tree[1]).append("\n");
+				sb.append(segmentTree[1]).append("\n");
 			}
 		}
+
 		System.out.println(sb);
 	}
 
-	private static int init(int start, int end, int node) {
+	private static int initSegmentTree(int start, int end, int node) {
 		if (start == end) {
-			return tree[node] = start;
+			return segmentTree[node] = start;
 		}
 		int mid = (start + end) / 2;
-		return tree[node] = minIndex(init(start, mid, node * 2), init(mid + 1, end, node * 2 + 1));
+		return segmentTree[node] = getMinIndex(initSegmentTree(start, mid, node * 2), initSegmentTree(mid + 1, end, node * 2 + 1));
 	}
 
-	private static int minIndex(int x, int y) {
-		if (arr[x] == arr[y]) {
+	private static int getMinIndex(int x, int y) {
+		if (originalArray[x] == originalArray[y]) {
 			return Math.min(x, y);
 		} else {
-			return arr[x] < arr[y] ? x : y;
+			return originalArray[x] < originalArray[y] ? x : y;
 		}
 	}
 
-	private static int update(int start, int end, int node, int index) {
+	private static int updateSegmentTree(int start, int end, int node, int index) {
 		if (start > index || end < index) {
-			return tree[node];
+			return segmentTree[node];
 		}
 		if (start == end) {
-			return tree[node] = index;
+			return segmentTree[node] = index;
 		}
 		int mid = (start + end) / 2;
-		return tree[node] = minIndex(update(start, mid, node * 2, index), update(mid + 1, end, node * 2 + 1, index));
+		return segmentTree[node] = getMinIndex(updateSegmentTree(start, mid, node * 2, index), updateSegmentTree(mid + 1, end, node * 2 + 1, index));
 	}
 }
