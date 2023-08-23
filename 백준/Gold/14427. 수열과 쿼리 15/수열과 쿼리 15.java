7fc7f9da7a0b7e2785edd076fbdb3fbf3a818ca8
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-08-23 16:49:05 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 656 ms, Memory: 47064 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
@@ -1,60 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] ar;
-    static PriorityQueue<Node> minHeap = new PriorityQueue<>((a, b) -> {
-        if (a.value == b.value) {
-            return a.index - b.index;
-        }
-        return Long.compare(a.value, b.value);
-    });
+	static int[] arr;
+	static int[] tree;
 
-    static class Node {
-        int index;
-        long value;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		arr = new int[n + 1];
+		tree = new int[n * 4];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		init(1, n, 1);
+		int m = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			if (a == 1) {
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				arr[b] = c;
+				update(1, n, 1, b);
+			} else {
+				sb.append(tree[1]).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
 
-        public Node(int index, long value) {
-            this.index = index;
-            this.value = value;
-        }
-    }
+	private static int init(int start, int end, int node) {
+		if (start == end) {
+			return tree[node] = start;
+		}
+		int mid = (start + end) / 2;
+		return tree[node] = minIndex(init(start, mid, node * 2), init(mid + 1, end, node * 2 + 1));
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        ar = new int[N];
+	private static int minIndex(int x, int y) {
+		if (arr[x] == arr[y]) {
+			return Math.min(x, y);
+		} else {
+			return arr[x] < arr[y] ? x : y;
+		}
+	}
 
-        for (int i = 0; i < N; i++) {
-            ar[i] = Integer.parseInt(st.nextToken());
-            minHeap.offer(new Node(i, ar[i]));
-        }
-
-        int M = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int q = Integer.parseInt(st.nextToken());
-
-            if (q == 1) {
-                int a = Integer.parseInt(st.nextToken()) - 1;
-                int b = Integer.parseInt(st.nextToken());
-
-                ar[a] = b;
-                minHeap.offer(new Node(a, b));
-            } else {
-                while (!minHeap.isEmpty() && ar[minHeap.peek().index] != minHeap.peek().value) {
-                    minHeap.poll();
-                }
-                sb.append(minHeap.peek().index + 1).append("\n");
-            }
-        }
-
-        System.out.println(sb);
-    }
+	private static int update(int start, int end, int node, int index) {
+		if (start > index || end < index) {
+			return tree[node];
+		}
+		if (start == end) {
+			return tree[node] = index;
+		}
+		int mid = (start + end) / 2;
+		return tree[node] = minIndex(update(start, mid, node * 2, index), update(mid + 1, end, node * 2 + 1, index));
+	}
 }
