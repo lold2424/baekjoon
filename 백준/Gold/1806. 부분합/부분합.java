045d0b0c5ef1cac02e48b970acd79430b0f65604
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-08-27 12:30:12 +0900

    [Gold IV] Title: 부분합, Time: 304 ms, Memory: 24320 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int[] numbers = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int left = 0, right = 0, minLength = N + 1, currentSum = numbers[0];
+		while (right < N && left <= right) {
+			if (currentSum >= S) {
+				minLength = Math.min(minLength, right - left + 1);
+				currentSum -= numbers[left++];
+			} else {
+				right++;
+				if (right < N) {
+					currentSum += numbers[right];
+				}
+			}
+		}
+
+		if (minLength > N) {
+			minLength = 0;
+		}
+
+		System.out.println(minLength);
+	}
+}
