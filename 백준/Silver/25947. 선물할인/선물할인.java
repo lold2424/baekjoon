89c855d5ec17141498310d8148b1f672718b0b92
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-10-15 10:29:37 +0900

    [Silver I] Title: 선물할인, Time: 484 ms, Memory: 37068 KB -BaekjoonHub

--- 백준/Silver/25947. 선물할인/선물할인.java
+++ 백준/Silver/25947. 선물할인/선물할인.java
@@ -1,63 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-    int n, b, a;
-    int nums[];
-    public static void main(String args[]) throws IOException {
-        Main m = new Main();
-        m.sol();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    }
-    public int getN(){
-        boolean temp = true;
-        int right =0;
-        int left =0;
-        for(int i = 0 ;i < a ; i++){
-            b-=nums[i]/2;
-            right = i+1;
-            if(b < 0){
-                temp = false;
-                return i;
+        String[] input = br.readLine().split(" ");
+        int numItems = Integer.parseInt(input[0]);
+        int budget = Integer.parseInt(input[1]);
+        int maxDiscountedItems = Integer.parseInt(input[2]);
+
+        int[] itemPrices = new int[numItems + 1];
+
+        input = br.readLine().split(" ");
+        for (int i = 1; i <= numItems; i++) {
+            itemPrices[i] = Integer.parseInt(input[i - 1]);
+        }
+
+        Arrays.sort(itemPrices);
+
+        int result = -1;
+        int[] dp = new int[100001];
+        dp[0] = 0;
+
+        for (int i = 1; i <= maxDiscountedItems; i++) {
+            dp[i] = dp[i - 1] + (itemPrices[i] / 2);
+            if (dp[i] > budget) {
+                result = i - 1;
+                break;
             }
         }
-        if(temp){
-            while(right < n){
 
-                if(right - left < a){
-                    b-=nums[right]/2;//할인된가격 산다.
-                    if(b < 0) break;
-                    right++;
-                }else{
-                    if(a >0)b-=nums[left++]/2;
-                    else {
-                        b-= nums[right];
-                        if(b < 0)break;
-                        right++;
-                    }
+        if (result == -1) {
+            for (int i = maxDiscountedItems + 1; i < 100000; i++) {
+                if (i > numItems) {
+                    result = i - 1;
+                    break;
+                }
+                dp[i] = dp[i - 1] + (itemPrices[i] / 2);
+                dp[i] += itemPrices[i - maxDiscountedItems] / 2;
+                if (dp[i] > budget || i > numItems) {
+                    result = i - 1;
+                    break;
                 }
             }
-            return right;
-        }
-        return right;
-    }
-    public void sol() throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String q = br.readLine();
-        n= Integer.parseInt(q.split(" ")[0]);
-        b= Integer.parseInt(q.split(" ")[1]);
-        a = Integer.parseInt(q.split(" ")[2]);//최대 선물의 수d
-        nums = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0; i< n;i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(nums);
-        System.out.println(getN());
+        System.out.println(result);
     }
-
-
 }
