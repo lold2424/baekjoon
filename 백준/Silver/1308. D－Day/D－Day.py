0d737fbe17a23ef686bb89f18e71c1b87b94dd7f
윤혜진 <91832324+lold2424@users.noreply.github.com>
2023-11-28 14:39:08 +0900

    [Silver V] Title: D-Day, Time: 68 ms, Memory: 38052 KB -BaekjoonHub

---
+++ 백준/Silver/1308. D－Day/D－Day.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+today = list(map(int, input().split()))
+d_day = list(map(int, input().split()))
+
+if today[0] + 1000 < d_day[0] or (today[0] + 1000 == d_day[0] and (today[1], today[2]) <= (d_day[1], d_day[2])):
+    print("gg")
+else:
+    today = date(*today)
+    d_day = date(*d_day)
+    print("D-"+str(d_day.toordinal() - today.toordinal()))
