4579ecf6bca5cb49f11cad02e3c32856af53599f
HyeJinYoon <91832324+lold2424@users.noreply.github.com>
2025-01-24 18:37:47 +0900

    [Silver I] Title: 팔, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		String left = st.nextToken();
+		String right = st.nextToken();
+		int count = 0;
+		
+		if (left.length() == right.length()) {
+			for (int i = 0; i < left.length(); i++) {
+				if (left.charAt(i) != right.charAt(i)) {
+					break;
+				}
+				else {
+					if (left.charAt(i) == '8') {
+						count ++;
+					}
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
