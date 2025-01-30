4569f6b56a7ac38e8d80f8ca3bb12831d716d6fc
HyeJinYoon <91832324+lold2424@users.noreply.github.com>
2025-01-30 18:54:49 +0900

    [Silver I] Title: Hurry the Hedgehog, Time: 1072 ms, Memory: 285960 KB -BaekjoonHub

--- 백준/Silver/33254. Hurry the Hedgehog/Hurry the Hedgehog.java
+++ 백준/Silver/33254. Hurry the Hedgehog/Hurry the Hedgehog.java
@@ -1,5 +1,13 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
