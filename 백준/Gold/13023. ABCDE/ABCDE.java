d7346ab835081f70624bcbc16dd33b314056b5cc
HyeJinYoon <91832324+lold2424@users.noreply.github.com>
2025-02-03 19:08:38 +0900

    [Gold V] Title: ABCDE, Time: 216 ms, Memory: 20660 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static List<Integer>[] adj;
+    static boolean[] visited;
+    static boolean found;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        adj = new ArrayList[N];
+        for(int i = 0; i < N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adj[a].add(b);
+            adj[b].add(a);
+        }
+        
+        visited = new boolean[N];
+        found = false;
+        
+        for(int i = 0; i < N; i++){
+            visited[i] = true;
+            dfs(i, 0);
+            visited[i] = false;
+            
+            if(found) break;
+        }
+        
+        System.out.println(found ? 1 : 0);
+    }
+    
+    static void dfs(int current, int depth){
+        if(depth == 4) {
+            found = true;
+            return;
+        }
+        
+        for(int next : adj[current]) {
+            if(!visited[next]) {
+                visited[next] = true;
+                dfs(next, depth + 1);
+                visited[next] = false;
+                
+                if(found) return;
+            }
+        }
+    }
+}
