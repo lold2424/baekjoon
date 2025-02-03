e1667127e5537908248ccdd637ab71e9e69cd184
HyeJinYoon <91832324+lold2424@users.noreply.github.com>
2025-02-03 19:26:04 +0900

    [Gold V] Title: ABCDE, Time: 252 ms, Memory: 20296 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
