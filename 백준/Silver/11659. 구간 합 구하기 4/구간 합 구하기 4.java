69b0ab85382999a778639249733ba10343f637b6
HyeJinYoon <91832324+lold2424@users.noreply.github.com>
2025-02-26 22:31:21 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 500 ms, Memory: 48308 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -6,29 +6,45 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		int[] firstLine = readFirstLine(br);
+		int N = firstLine[0], M = firstLine[1];
 		
+		int[] num = readNumbers(br, N);
+		int[] prefixSum = computePrefixSum(num, N);
+		
+		processQueries(br, M, prefixSum);
+	}
+	
+	private static int[] readFirstLine(BufferedReader br) throws IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
+		return new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+	}
+	
+	private static int[] readNumbers(BufferedReader br, int N) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] num = new int[N + 1];
-		int[] prefixSum = new int[N + 1];
-		
-		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
+		}
+		return num;
+	}
+	
+	private static int[] computePrefixSum(int[] num, int N) {
+		int[] prefixSum = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
 			prefixSum[i] = prefixSum[i - 1] + num[i];
 		}
-		
+		return prefixSum;
+	}
+	
+	private static void processQueries(BufferedReader br, int M, int[] prefixSum) throws IOException {
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			
 			sb.append(prefixSum[y] - prefixSum[x - 1]).append("\n");
 		}
-		
 		System.out.print(sb.toString());
 	}
 }
